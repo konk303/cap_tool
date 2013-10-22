bb63d96ab07c1657617e8b9cf04bf6dac7169bf9
Kimihiko NAKAMURA <kimihiko.nakamura.fz@ps.hitachi-solutions.com>
2013-10-22 15:51:24 +0900

    new task to upload tar of repo to remote.

--- lib/dsl_paths_patch.rb
+++ lib/dsl_paths_patch.rb
@@ -1,21 +1,35 @@
 # -*- coding: utf-8 -*-
 
-# == override path related methods to handle multiple repos/targets.
+# == override path related methods to handle multiple repos per one role.
 module Capistrano
   module DSL
-    def deploy_path
+    def current_path
       repo = fetch(:repo)
-      repo ? super.join(repo.to_s) : super
+      repo ? deploy_path.join(repo.to_s) : super
     end
 
     def releases_path
       repo = fetch(:repo)
-      repo ? Pathname.new(deploy_to).join('releases', repo.to_s) : super
+      repo ? deploy_path.join('releases', repo.to_s) : super
+    end
+
+    def release_path
+      repo = fetch(:repo)
+      repo ? fetch(:"#{repo}_release_path", current_path) : super
+    end
+
+    def set_release_path(timestamp = now)
+      repo = fetch(:repo)
+      repo ? set(:"#{repo}_release_path", releases_path.join(timestamp)) : super
     end
 
     def shared_path
       repo = fetch(:repo)
-      repo ? Pathname.new(deploy_to).join('shared', repo.to_s) : super
+      repo ? deploy_path.join('shared', repo.to_s) : super
+    end
+
+    def now
+      env.timestamp.strftime("%Y%m%d%H%M")
     end
   end
 end
