12f78a2ed1856632c25491033035a412ee647ab8
Kimihiko NAKAMURA <kimihiko.nakamura.fz@ps.hitachi-solutions.com>
2013-10-24 16:46:22 +0900

    added restart task

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,4 +7,8 @@ set :linked_dirs, ["bundler", "log", "wk_app", "wk_sysop"]
 
 ssh_proxy = Net::SSH::Proxy::HTTP.new(fetch(:proxy_host), fetch(:proxy_port),
   :user => fetch(:proxy_user), :password => fetch(:proxy_pass))
-set :ssh_options, { :proxy => ssh_proxy}
+set :ssh_options, {
+  :proxy => ssh_proxy,
+  :keepalive => true,
+  :keepalive_interval => 30
+}
